1d95fbdd56c2d1dae413c98b5c3598abe7fd0003
Kseniya <159527696+Kseniya-Igorivna@users.noreply.github.com>
2024-07-02 19:57:33 +0300

    313

--- test.js
+++ test.js
@@ -1,3 +1,11 @@
-const planets = ['Earth', 'Mars', 'Venus'];
-console.log(planets[3]); // undefined
-console.log(planets[999]); // undefined
+const p1 = new Promise((resolve, reject) => {
+  setTimeout(() => resolve(1), 2000);
+});
+
+const p2 = new Promise((resolve, reject) => {
+  setTimeout(() => rejected(2), 1000);
+});
+
+Promise.race([p1, p2])
+	.then(value => console.log(value)); 
+	.catch(error => console.log(error)); // 2
