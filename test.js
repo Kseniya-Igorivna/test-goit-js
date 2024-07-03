6906944b7b6aecfb9af994939bfa1f71bdce1058
Kseniya <159527696+Kseniya-Igorivna@users.noreply.github.com>
2024-07-03 18:26:07 +0300

    2013

--- test.js
+++ test.js
@@ -1,11 +1,10 @@
-const p1 = new Promise((resolve, reject) => {
-  setTimeout(() => resolve(1), 2000);
-});
-
-const p2 = new Promise((resolve, reject) => {
-  setTimeout(() => rejected(2), 1000);
-});
-
-Promise.race([p1, p2])
-	.then(value => console.log(value)); 
-	.catch(error => console.log(error)); // 2
+fetch("https://jsonplaceholder.typicode.com/users")
+  .then(response => {
+    // Response handling
+  })
+  .then(data => {
+    // Data handling
+  })
+  .catch(error => {
+    // Error handling
+  });
